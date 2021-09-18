8492435cb9e876ef3c4191eb2dfb12f8c823a94c
RishmithaChennupati <89614712+RishmithaChennupati@users.noreply.github.com>
2021-09-17 23:02:33 -0500

    Hover and GitHub USer

--- ICP-4/Source/Hover/hover.js
+++ ICP-4/Source/Hover/hover.js
@@ -9,7 +9,6 @@ function upDate(previewPic) {
     console.log(previewPic);
     $("#image").css('background-image', 'url(' + previewPic.src + ')');
     $('#image').html(previewPic.alt);
-
 }
 
 function unDo() {
@@ -22,5 +21,4 @@ function unDo() {
    */
     $("#image").css('background-image','none');
     $('#image').html("Hover over an image below to display here.");
-
 }
